ec1a04c532650afe7dc500d07c7ecd61149dcda8
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-08-20 15:36:54 +0000

    fixed Lines

---
+++ M1T1_Hello World.cpp
@@ -0,0 +1,19 @@
+/*
+CSC 134
+M1T1 - Hello World 
+Curtis Cooper 8/20/25
+First CSC programm.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ cout << "Hello, World!" << endl;  
+ couit << "My foo is Gaming" 
+ return 0;
+}
+
+
+
+
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ CSC134 C++ Programing
 
 This is C++ repository for CSC134 
 
-first code: Hello World
+First code: Hello World
