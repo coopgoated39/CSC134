d2a85ba7df99566733cbf6d924317587317793c1
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-08-25 15:11:46 +0000

    added lines

--- M1T1.cpp
+++ M1T1.cpp
@@ -1,3 +1,10 @@
+/*
+Input: run the program
+Process: none yet
+OUTPUT: return 0 (no error)
+SPECIAL FX: Greet the user. 
+*/
+
 //magic word to start
 #include <iostream>
 using namespace std; int main() {
