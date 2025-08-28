e3b3f3f5eacd942742f68a47be9a364cb2161387
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-08-28 19:21:00 +0000

    edited line 19

--- M1HW1.cpp
+++ M1HW1.cpp
@@ -10,10 +10,10 @@ using namespace std;
 int main() {
  string bestmovie = "Avengers";
  string releasedate = "May 4, 2012";
- int ticketssold = 27000000;
+ int ticketssold = 806000;
  double priceperticket = 8.00; 
  double approximategross = priceperticket * ticketssold;   
 
  cout << "One of my favorite movies from Marvel is " << bestmovie << endl; 
  cout << "It was released " << releasedate << endl;
- cout << "In the first weekend, The movie made approximately $" << approximategross;}
+ cout << "In the first weekend, The movie made approximately in the state of North Carolina $" << approximategross;}
