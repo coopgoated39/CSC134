f33eb16b5c62a2b5fd94124d0919389d173ea1ad
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-08-28 18:00:26 +0000

    fixing gramar

---
+++ M1LAB.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134
+M1LAB - applesales 
+Curtis Cooper 8/28/25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ string name = "Curtis Cooper";
+ int apples = 100;
+ double priceperApple= .25; 
+
+ cout << "Welcome to " << name;
+ cout << " 's Orchard" << endl;
+
+}
