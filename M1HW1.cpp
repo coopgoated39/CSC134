1f066d5c28c72ba98f91666bc9b1a871a0e61bf2
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-08-28 19:01:46 +0000

    fixed erros in line 19 and 17

---
+++ M1HW1.cpp
@@ -0,0 +1,19 @@
+/*
+CSC 134
+M1HW1 - Movies  
+Curtis Cooper 8/28/25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ string bestmovie = "Avengers";
+ string releasedate = "May 4,2012";
+ int ticketssold = 27000000;
+ double priceperticket = 8.00; 
+ double approximategross = priceperticket * ticketssold;   
+
+ cout << "One of my favorite movies from Marvel is " << bestmovie << endl; 
+ cout << "It was released " << releasedate << endl;
+ cout << "In the first weekend, The movie made approximately " << approximategross;}
