10b7eb60e2e4f85ac8493b490deef2e761d3477c
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-14 17:46:20 +0000

    fixed structure

---
+++ M2T2.cpp
@@ -0,0 +1,27 @@
+/*
+CSC 134,M2T1
+Curtis Cooper
+9/14/25
+Receipt Calculator.
+*/
+# include <iostream>
+ using namespace std;
+
+ int main () {
+    //Define varibles//
+    double price_before_tax = 5.99;
+    double tax_amount =.08;
+    
+    //calculate//
+    double amount_of_tax = price_before_tax * tax_amount;
+    double total_price = price_before_tax + tax_amount;
+    
+    //display results//
+    cout << tax_amount << " % Taxes on an item costing $" << price_before_tax << end1;
+    cout << "is $" amount_of_tax << end1;
+    cout << "The total price is $" << total_price << end1;
+ }
+
+
+
+
