3eca275a22510b4cbb520facdc5340fb76c679d0
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-14 17:58:33 +0000

    structure

--- M2T2.cpp
+++ M2T2.cpp
@@ -18,7 +18,7 @@ Receipt Calculator.
     
     //display results//
     cout << tax_amount << " % Taxes on an meal costing $" << price_before_tax <<
-    "is $" << amount_of_tax << endl;
+    " is $" << amount_of_tax << endl;
     cout << "The total price for the meal $" << total_price << endl;
  }
 
