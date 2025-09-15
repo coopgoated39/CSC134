5809b32cc611fb8ed75e4f88b0f4edf77a8c2531
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-15 15:04:05 +0000

    adding instructions

---
+++ M2HW1.cpp
@@ -0,0 +1,30 @@
+/*
+M2HW1
+Curtis Cooper 
+9/15/2025
+*/
+
+#include <iostream>
+using namespace std; int main() {
+
+    void question1 () {
+        cout << "Question 1" <<
+
+    void question2 () {
+        cout << "Question2" <<
+
+    void question3 () {
+        cout << "Question3" <<
+        
+    void question4 () {
+        cout << "Question4" <<
+
+    int main() {
+        //call each question its own function
+
+        question1 ()
+        Question2 ()
+
+        cout 
+
+    
