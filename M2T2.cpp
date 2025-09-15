39605c6b99eb0f94e7f99fc42282761b81c87c8f
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-15 15:11:28 +0000

    used precision

--- M2T2.cpp
+++ M2T2.cpp
@@ -5,6 +5,7 @@ Curtis Cooper
 Receipt Calculator.
 */
 # include <iostream>
+# include <iomanip>
  using namespace std;
 
  int main () {
@@ -17,6 +18,7 @@ Receipt Calculator.
     double total_price = price_before_tax + amount_of_tax;
     
     //display results//
+    cout << setprecision(2) << fixed;
     cout << tax_amount << " % Taxes on a meal costing $" << price_before_tax <<
     " is $" << amount_of_tax << endl;
     cout << "The total price for the meal is $" << total_price << endl;
