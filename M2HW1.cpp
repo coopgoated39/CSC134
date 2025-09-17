6c74663cb0657f8552440f8e21107d90398db34c
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-17 15:55:11 +0000

    need to finish

--- M2HW1.cpp
+++ M2HW1.cpp
@@ -1,30 +1,70 @@
 /*
-M2HW1
+M2HW1 - Gold
 Curtis Cooper 
 9/15/2025
 */
 
 #include <iostream>
-using namespace std; int main() {
+#include <string>
+#include <cstdlib>
+using namespace std;
 
-    void question1 () {
-        cout << "Question 1" <<
+void question1() {
+    cout << "Question 1" << endl;
+}
+int main() {
+        // define varibles
+        double starting_account_balance;
+        double deposit_amount;
+        double withdrawl_amount;
+        string account_name;
+        
+        //get user input
+        cout << "What is the name on the account ?" << endl;
+        cin >> account_name;
+        cout << "Hello" << account_name << endl;
+        cout << "what is the account balance" << endl;
+        cin >> starting_account_balance; 
+        cout << "How much money does "  << account_name << "want to deposit?" << endl;
+        cin >> deposit_amount;
+        cout << "How much money does " << account_name << "want to withdraw?" << endl;
+        cin >> withdrawl_amount;
+        cout << rand(); // TODO
+        //calculations
+        final_account_balance = starting_account_balance = deposit_amount - withdrawl_amount
+        
+        //print statements
+        cout << account_name << endl;
+        cout << "-------------------" << endl;
+        cout << "Account Number: " << rand() << endl;
+        cout <<"--------------------" << endl;
+        cout << final_account_balance << endl;
+
+
+
+}
+
 
     void question2 () {
         cout << "Question2" <<
+    }
 
     void question3 () {
         cout << "Question3" <<
+    }    
         
     void question4 () {
         cout << "Question4" <<
+    }
 
     int main() {
         //call each question its own function
+    }
 
         question1 ()
         Question2 ()
 
         cout 
+}
 
     
