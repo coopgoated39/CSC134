862a07714647de4a9ef5283af3714439661f386b
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-20 18:22:19 +0000

    updated to answerassignment question

--- M2HW1.cpp
+++ M2HW1.cpp
@@ -17,37 +17,86 @@ int main() {
         double starting_account_balance;
         double deposit_amount;
         double withdrawl_amount;
+        double final_account_balance
         string account_name;
         
         //get user input
         cout << "What is the name on the account ?" << endl;
         cin >> account_name;
-        cout << "Hello" << account_name << endl;
+        cout << "Hello " << account_name << endl;
         cout << "what is the account balance" << endl;
         cin >> starting_account_balance; 
         cout << "How much money does "  << account_name << "want to deposit?" << endl;
         cin >> deposit_amount;
         cout << "How much money does " << account_name << "want to withdraw?" << endl;
         cin >> withdrawl_amount;
-        cout << rand(); // TODO
+        //cout << rand(); // TODO
+
         //calculations
-        final_account_balance = starting_account_balance = deposit_amount - withdrawl_amount
+        final_account_balance = starting_account_balance + deposit_amount - withdrawl_amount;
         
         //print statements
         cout << account_name << endl;
         cout << "-------------------" << endl;
         cout << "Account Number: " << rand() << endl;
         cout <<"--------------------" << endl;
-        cout << final_account_balance << endl;
+        cout <<"Final Balance $" << final_account_balance << endl;
 
-
-
+        return 0;
 }
 
 
     void question2 () {
         cout << "Question2" <<
     }
+
+# include <iostream>
+# include <iomanip>
+using namespace std; 
+
+int main() 
+{
+    // Constansts for cost and amount charged 
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.52; 
+    
+    //Varibles
+    double length; //The crates length
+    double width; //The crates width
+    double height; //The crates heght 
+    double volume; //The crates volume
+    double cost;  //The cost to build the crates
+    double charge; //The customer charge for the crate
+    double profit; // the profit made on the crate
+
+    
+    // Set the desired output formating for numbers.  
+    cout << setprecision(2) << fixed << showpoint;
+    
+    //Prompt the user for the crates length, width,and height
+    cout << "Enter the dimensions of the crate (in feet): \n"; 
+    cin >> length; 
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: "; 
+    cin >> height; 
+    
+    //Calculate the crates's volume, the cost to produce it
+    // the charge to the customer, and the profit
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT; 
+    profit = charge - cost;
+
+    //Display Calculated data
+    cout << "The volume of the crate is "; 
+    cout << volume << " cubic feet. \n";
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to cutomer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
+    return 0;
+
+}
 
     void question3 () {
         cout << "Question3" <<
