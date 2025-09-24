3660f743f9be7789f5ae9556a29f935e59ac6779
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-24 15:47:39 +0000

    saving for later

---
+++ M3LAB1.cpp
@@ -0,0 +1,87 @@
+/*
+CSC 134,M3LAB1
+Curtis Cooper
+9/24/25
+Game.
+*/
+
+// this game will include options person who want to take shortcuts in life instead of working towards what they want to do.
+
+# include <iostream>
+#include <ctime>
+#include <cstdlib>
+ using namespace std;
+ 
+ // ========== FUNCTION PROTOTYPES ==========
+// Declare all your "rooms" up here
+int start = 1; 
+void Doctor(); // A possible path
+void Depressed_worker(); // Another path
+void Criminal(); // possible path
+void gameOver(); // An ending
+void victory(); // Another ending
+int roll(); //funtion for dice roll
+
+int roll() {
+    int roll = (rand() % 3) + 1;
+    return roll;
+}
+
+// ========== MAIN FUNCTION ==========
+int main()
+{
+    srand(static_cast<unsigned int>(time(0)));
+
+    cout << "You have chosen to come see the wonderful wizard of oz" << endl;
+    cout << "You have expressed a disire to forgo the rest of your FTCC classes for a chance to expedite your success" << endl;
+    cout << "This is a risk you are prepared to take" << endl;
+    cout << " Be warned avoiding hard work may come with a price" endl; 
+    cout << "==================================" << endl;
+    cout << " Here is the Dice it is your time to fulfil your destiny ! " << endl;
+    cout << "==================================" << endl;
+    cout << "when ready to start just type 1 ?";
+    cin >> start;
+    if (start == 1) { // Compare against the value 1
+        int diceRoll = roll(); // Call the function and store the result
+        cout << "You rolled a " << diceRoll << "!" << endl; 
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cout << endl;
+// Start the adventure!
+tavern();
+cout << "\n=== THE END ===" << endl;
+return 0;
+}
+// ========== LOCATION FUNCTIONS ==========
+// Each function is one "node" in your story
+void tavern()
+{
+cout << "\nYou wake up in a dusty tavern." << endl;
+cout << "The bartender says there's trouble in the kingdom." << endl;
+cout << "Will you help? (1 = Yes, 2 = No): ";
+int choice;
+cin >> choice;
+if (choice == 1)
+{
+cout << "\n'Brave soul! Head to the forest!'" << endl;
+forest(); // Go to forest function
+}
