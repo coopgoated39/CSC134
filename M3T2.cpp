93574c22358b8b614808480351ca2f07f54efe3c
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-09-28 17:18:04 +0000

    fixing errors

---
+++ M3T2.cpp
@@ -0,0 +1,64 @@
+/*
+CSC 134,M3LAB1
+Curtis Cooper
+9/28/25
+Game.
+*/
+
+// this is crapp game that gives player the option to keep playing.
+
+# include <iostream>
+#include <cmath>
+#include <ctime>
+#include <string>
+ using namespace std;
+
+ int main() {
+    const int SIDES = 6; // yours will differ!
+
+    //int seed = 19;
+    int seed = time(0);
+
+    // seed the random number generator
+    srand(seed);
+
+    // rand() is a large number, so we take the remainder which is %
+    int roll1, roll2, total;
+    char answer;
+
+    do {
+         // roll two dice
+         roll1 = ( (rand() % SIDES)+1 );
+         cout <<"Your roll is: " << roll1 << endl;
+         
+         roll2 = ((rand() % SIDES)+1 );
+         cout <<"Your roll is: " << roll2 << endl;
+         total = roll1 + roll2;
+         
+         //declare the rules and give options to keep rolling dice
+         if (total == 7 || total == 11) {
+            cout <<"You won: Would you like to roll again? (y/n)" << endl;
+            cin >> answer;
+        }
+        
+        else if (total == 2 ) {
+            cout << "Snake Eyes, You crapped out, Would you like to roll again? (y/n)" << endl;
+            cin >> answer;
+        }
+        else if (total != 2 && total != 7 && total != 11) {
+            cout << "You crapped out looser, Would you like to roll again? (y/n)" << endl;
+            cin >> answer;
+        }
+    }
+    //continue game until customer wants to stop
+    while (answer == 'y' || answer == 'Y');
+
+    //exit statement
+    cout << "Thanks for playing !" << endl;
+
+    return 0;
+        
+    
+    
+
+ }
