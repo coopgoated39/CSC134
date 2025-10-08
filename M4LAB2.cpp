3f65a641ba6abf125efc752c8a3b1edf8754c18c
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-10-08 14:45:37 +0000

    save

---
+++ M4LAB2.cpp
@@ -0,0 +1,33 @@
+/*
+CSC 134,M4LAB2
+Curtis Cooper
+9/24/25
+Game.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //declare varibles
+    int length = 10;
+    int width = 10;
+    string tile = 🎃;
+
+    //print horizonally
+    for (int i = 0; i < length; i++) {
+        cout << tile;
+    }
+    cout << endl;
+
+    //Example 2: Same thing, with a while loop
+    int ount = 0; 
+    while (count < length) {
+        cout << title;
+        count++; //count from 0 to just before length
+
+    }
+    cout << endl;
+
+}
