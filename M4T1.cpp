bf27c77eeee8d5d1a143bf39d776b97f121eb551
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-10-12 20:55:41 +0000

    it works

---
+++ M4T1.cpp
@@ -0,0 +1,20 @@
+/* CSC 134
+ M4T1 Loop
+ Curtis Cooper
+ 12 OCT 2025
+*/
+
+#include <ostream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int count = 1;
+    while (count < 6) {
+        cout << "Hello number " << count << endl;
+        count ++;
+    }
+
+    return 0;
+}
