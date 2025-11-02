3ee79a6269016881bb8da14767d3e404a18099f4
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-11-02 20:06:29 +0000

    it works

---
+++ M4HW1.cpp
@@ -0,0 +1,27 @@
+/* CSC 134
+ M4HW1 - Bronze
+ Curtis Cooper
+ 2 NOV 2025
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    // *******Question 1 *****
+
+    int number = 5;   // The number for the times table
+    int i = 1;        // Counter starting at 1
+
+    // While loop to print 5 times table from 1 to 12
+    while (i <= 12) {
+        cout << number << " times " << i << " is " << number * i << "." << endl;
+        i++; // Increment counter
+    }
+    
+    return 0;
+}
