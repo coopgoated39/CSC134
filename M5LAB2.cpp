c3fbd0974d90ff4137c9b3b63f4ddb4c2d115fe9
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-11-03 15:56:11 +0000

    fixinfg errors

--- M5LAB2.cpp
+++ M5LAB2.cpp
@@ -1,13 +1,79 @@
 /* CSC 134
- M4HW1 - Bronze
+ M5LAB2 
  Curtis Cooper
- 2 NOV 2025
+ 3 NOV 2025
 */
 
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
 using namespace std;
 
-int main() {
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+
+double get_length(); 
+double get_width();
+double get_area(double length, double width);
+void display_data(double length, double length, double area);
+
+
+int main()
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = get_length();
+   
+   // Get the rectangle's width.
+   width = get_width();
+   
+   // Get the rectangle's area.
+   area = get_area();
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
+
+//get_length - Ask the user to enter a rectangle's length, and return that value as a double.
+
+double get_length(){
+    double length;
+    cout << "what is length ?" << endl;
+    cin >> length;
+    return length;
+}
+
+//get_width - Ask the user to enter a rectangle's width, and return that value as a double.
+double get_width() {
+    double width;
+    cout << "What is the width" << endl;
+    cin >> width;
+    return width;
+}
+
+// get area - This  function should take two arguments, length and width. It will calculate the area and return that value as a double.
+double get_area(double length, double width){
+    double area;
+    area = length * width;
+    return area;
+}
+
+// display data - Void funtion, simply outputs the values in a readable format.
+void display_data(double length, double length, double area) {
+    cout << "Rectangle is " << length << "by" << width << "." << endl;
+    cout << "Area is: " << area << endl;
+    return; //reutrn is assumed for a void funtion.
+}
