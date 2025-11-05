5df4c9e0ed5702d6fc816e0a7f0cd951a35c4b0f
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-11-05 15:38:50 +0000

    implementing changes

---
+++ M5LAB1.cpp
@@ -0,0 +1,45 @@
+/* CSC 134
+ M5LAB1
+ Curtis Cooper
+ 5 NOV 2025
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function declarations
+int getPlayerChoice(int maxChoice); // let player choose options
+
+
+// main()
+int main() {
+    int choice;
+    int max = 3;
+    cout << "TESTING: Choose 1, 2, or 3." << endl;
+    choice = getPlayerChoice(max);
+    cout << "You chose: " << choice << endl;
+
+    // ending
+    return 0;
+}
+
+// Function definitions
+/**
+ * Get a valid choice from the player.
+ * example: if maxChoice is 3, they can choose 1, 2, or 3.
+ */
+int getPlayerChoice(int maxChoice) {
+    int choice;
+    while (true) {
+        cout << "Your choice: ";
+        cin >> choice;
+        // You can add extra validation if you want.
+
+        // Validate range 
+        if (choice >= 1 && choice <= maxChoice) {
+            return choice; // same number they enter
+        }
+
+        cout << "Please choose between 1 and " << maxChoice << ".\n";
+    }
+}
