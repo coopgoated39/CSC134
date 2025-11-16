b2c9c4de6f5f2e8c89ababd0feb804c135d0cb2e
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2025-11-16 19:19:10 +0000

    maed fromatting changes to fit on olne page and corrected math it works

--- M5HW1.cpp
+++ M5HW1.cpp
@@ -9,11 +9,32 @@
 #include <iomanip>
 using namespace std;
 
+void question1 ();
+void question2 ();
+void question3 ();
 
 
-//Question 1
 
-int main() {
+int main() {    //I had to do it like this V so i would go on the page and not give errors
+
+    // Call Question 1
+    cout << "\n--- Question 1: Average Rainfall ---\n";
+    question1();
+
+    // Call Question 2
+    cout << "\n--- Question 2: Block Volume ---\n";
+    question2();
+
+    // Call Question 3
+    cout << "\n--- Question 3: ---\n";
+    question3();
+
+    return 0;
+}
+
+//*******************************Question1***********************
+
+void question1 () {    
     string month1, month2, month3;
     double rain1, rain2, rain3;
 
@@ -41,8 +62,83 @@ int main() {
     cout << "\nThe average rainfall for " 
          << month1 << ", " << month2 << ", and " << month3 
          << " is " << average << " inches.\n";
+         
+}
 
-    return 0;
+
+    //*************************Question2*************************
+
+    void question2 () {
+    double width, length, height;
+
+    // Ask for width
+    cout << "Enter the width of the block: ";
+    cin >> width;
+
+    // Validate width
+    while (width <= 0) {
+        cout << "Error, width must be greater than 0. Try again: ";
+        cin >> width;
+    }
+
+    // Ask for length
+    cout << "Enter the length of the block: ";
+    cin >> length;
+
+    // Validate length
+    while (length <= 0) {
+        cout << "Error, length must be greater than 0. Try again: ";
+        cin >> length;
+    }
+
+    // Ask for height
+    cout << "Enter the height of the block: ";
+    cin >> height;
+
+    // Validate height
+    while (height <= 0) {
+        cout << "Error, height must be greater than 0. Try again: ";
+        cin >> height;
+    }
+
+    // Calculate volume
+    double volume = width * length * height;
+
+    // Display result
+    cout << "\nThe volume of the block is: " << volume << endl;
+
+}
+
+//**************************Question3*****************************
+
+void question3 () {
+    int number;
+
+    // Ask user for input
+    cout << "Enter a number between 1 and 10: ";
+    cin >> number;
+
+    // Input validation
+    while (number < 1 || number > 10) {
+        cout << "Invalid input! Please enter a number between 1 and 10: ";
+        cin >> number;
+    }
+
+    // Display Roman numeral using switch
+    cout << "The Roman numeral version for " << number << " is: ";
+
+    switch(number) {
+        case 1: cout << "I"; break;
+        case 2: cout << "II"; break;
+        case 3: cout << "III"; break;
+        case 4: cout << "IV"; break;
+        case 5: cout << "V"; break;
+        case 6: cout << "VI"; break;
+        case 7: cout << "VII"; break;
+        case 8: cout << "VIII"; break;
+        case 9: cout << "IX"; break;
+        case 10: cout << "X"; break;
+    }
 }
 
 
